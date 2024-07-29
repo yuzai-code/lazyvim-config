1d1423eba4cd024c0a388eb9aec7e04271046abd
yuzai <958684803@qq.com>
2024-07-29 08:53:40 +0800

    修改K快捷键配置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,12 +5,20 @@
 -- 设置选项
 local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
+local unmap = vim.keymap.del
 
 -- 检查并取消现有映射
-vim.api.nvim_set_keymap("n", "J", "<NOP>", opts)
 
--- 设置新的映射
+-- ********设置新的映射*******
 map("i", "jj", "<Esc>", opts)
 map("i", "jk", "<Esc>", opts)
 map("n", "K", "5k", opts)
 map("n", "J", "5j", opts)
+
+-- 设置ctrl+q强制退出
+map("n", "<C-q>", ":qa!<CR>", opts)
+
+-- 设置全选
+map("n", "<C-a>", "ggVG", opts)
+map("i", "<C-a>", "<ESC>ggVG<CR>a", opts)
+map("v", "<C-a>", "ggVG", opts)
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  -- 禁用插件
+  -- 禁用 lsp客户端
+  {
+    "neovim/nvim-lspconfig",
+    enabled = false,
+  },
+}
