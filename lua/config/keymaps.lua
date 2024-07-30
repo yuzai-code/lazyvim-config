7bf93e5148881316ffd610e09604ba454b887faa
yuzai <958684803@qq.com>
2024-07-30 23:14:05 +0800

    修改行首行尾快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,3 +22,15 @@ map("n", "<C-q>", ":qa!<CR>", opts)
 map("n", "<C-a>", "ggVG", opts)
 map("i", "<C-a>", "<ESC>ggVG<CR>a", opts)
 map("v", "<C-a>", "ggVG", opts)
+
+-- 设置复制
+map("v", "<C-c>", '"+y', opts)
+map("n", "<C-c>", '"+y', opts)
+map("i", "<C-c>", '<ESC>"+y', opts)
+map("v", "<C-v>", '"+p', opts)
+map("n", "<C-v>", '"+p', opts)
+map("i", "<C-v>", '<ESC>"+p', opts)
+
+-- 设置行首行尾
+map("n", "H", "^", opts)
+map("n", "L", "$", opts)
