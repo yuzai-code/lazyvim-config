24436cc4da5de1bb366917b5f93501e6fe745515
yuzai-code <958684803@qq.com>
2024-08-05 01:02:55 +0800

    fix:修改lsp K按键
    其他案件功能正常使用

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,19 +4,7 @@ return {
   "neovim/nvim-lspconfig",
   opts = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-    -- Change K  to gh
-    for _, keymap in ipairs(keys) do
-      if keymap[1] == "K" then
-        keymap[1] = "gh" -- Change the key
-      end
-    end
-
-    -- Add a new keymap for diagnostics
-    table.insert(keys, { "gd", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>" })
-
-    -- Optionally disable a keymap by setting its command to false
-    -- Disable the "K" keymap
-    table.insert(keys, { "K", false })
+    -- disable a keymap
+    keys[#keys + 1] = { "K", false }
   end,
 }
