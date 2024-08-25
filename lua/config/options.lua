6980246e3300a240d23b878838309712ac3e22a3
yuzai-code <958684803@qq.com>
2024-08-25 13:35:19 +0800

    feat：neovide的配置

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,4 +10,19 @@ vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldenable = false
 -- 设置默认的折叠级别
 vim.opt.foldlevel = 99
--- 设置背景颜色级别
+-- 设置使用系统默认剪切板
+vim.opt.clipboard = "unnamedplus"
+
+-- nevide 的配置
+if vim.g.nevide then
+  -- 滚动动画长度
+  vim.g.neovide_scroll_animation_length = 0.1
+  -- 全屏
+  vim.g.neovide_fullscreen = true
+  -- 控制输入法
+  vim.g.neovide_input_ime = true
+  -- 光标动画长度
+  vim.g.neovide_cursor_animation_length = 0.1
+  -- 配置粒子特效
+  vim.g.neovide_cursor_vfx_mode = "torpedo"
+end
